89d65f8aee20681661c9e4dd5cc1b3a5c26339f4
Aashutosh Uikey <aashureal01@gmail.com>
2025-06-08 12:23:33 +0530

    new

--- routes/account.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Render Profile Page
-router.get("/", async (req, res) => {
-  res.render("account");
-});
-
-module.exports = router;
